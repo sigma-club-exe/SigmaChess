53a5e596c2ec400a9ff8e8d2edda922d5401dc3e
mefedraw <fyodor.dubkov@gmail.com>
2024-09-28 00:54:36 +0300

    5252:5252 remove

--- backend/ChessGame2.Tests/obj/Debug/net8.0/ChessGame2.Tests.AssemblyInfo.cs
+++ backend/ChessGame2.Tests/obj/Debug/net8.0/ChessGame2.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessGame2.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44bb96acd5aa453afbbe4482743301375a99534c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+716d472eb3063364ca7b126ba85d79e79aaae603")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessGame2.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessGame2.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
