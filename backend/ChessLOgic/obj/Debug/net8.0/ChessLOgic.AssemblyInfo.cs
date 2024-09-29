d06198e44f03614a748c4176b92d9b181a62d416
mefedraw <fyodor.dubkov@gmail.com>
2024-09-29 04:01:47 +0300

    connected send fixed

--- backend/ChessLOgic/obj/Debug/net8.0/ChessLOgic.AssemblyInfo.cs
+++ backend/ChessLOgic/obj/Debug/net8.0/ChessLOgic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessLOgic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9bc8547dfe48612f69c6fc3daf3b3819ff25fcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3510016d8805000d3883774013209c225b63a0a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessLOgic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessLOgic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
